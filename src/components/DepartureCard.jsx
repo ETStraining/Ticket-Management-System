2820fa63396a4c77cb36a710ce3b732b7f31bcf8
bosco250 <dusengimana06@gmail.com>
2024-08-20 17:42:27 +0200

    home changes

--- src/components/DepartureCard.jsx
+++ src/components/DepartureCard.jsx
@@ -10,13 +10,16 @@ function DepartureCard() {
   ];
 
   return (
-    <div className="max-w-sm rounded  shadow-lg p-4 m-4 bg-gray-300">
-      <div className="bg-indigo-500 text-center text-white text-lg font-bold py-2">
+    <div className="max-w-sm rounded  shadow-lg p-4 m-4 bg-gray-200">
+      <div className="bg-indigo-500 mt-[-16px] mx-[-16px]  rounded-t-lg">
+      <div className=" text-center text-white text-lg font-bold py-2">
         {route}
       </div>
-      <div className="bg-indigo-500 text-center text-white text-sm py-1">
+      <div className=" text-center text-white text-sm py-1">
         Departure: {frequency}
       </div>
+      </div>
+      
       <div className="py-4">
         <p className="text-gray-600 text-lg font-semibold">Price: {price}</p>
         <p className="text-gray-700 mt-2">Contact:</p>
@@ -27,7 +30,8 @@ function DepartureCard() {
         ))}
       </div>
       <Link to='/new-ticket'>
-      <button className='border-none px-4 py-2 bg-indigo-600 rounded-md text-white font-semibold'>Book now</button>
+      <button className='border-none px-4 py-2 bg-indigo-600 rounded-md text-white font-semibold
+      hover:bg-indigo-900'>Book now</button>
       </Link>
     </div>
   );
